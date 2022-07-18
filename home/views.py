02dcbee44fc030a06f0d565d2864b2c0f6f17f9a
Adithya Nandakumar <80444800+adithyanandakumar@users.noreply.github.com>
2022-07-18 23:20:33 +0530

    Added send_mail function (Gmail API)
    /api/sendmail/

--- home/views.py
+++ home/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.conf import settings
+from home.forms import SendForm
+import yagmail
 
-# Create your views here.
+
+def send_mail(request):
+    form = SendForm()
+    if request.method == 'POST':
+        form = SendForm(request.POST)
+        if form.is_valid():
+            recipient = form.cleaned_data.get('email')
+            yag = yagmail.SMTP(settings.EMAIL_HOST_USER, oauth2_file="./credentials.json")
+            yag.send(to=[recipient], subject="From Trebuchet 👋", contents="It works, dammit!")
+            messages.success(request, 'Success!')
+            return redirect('send_mail')
+    return render(request, 'sendMail.html', {'form': form})
