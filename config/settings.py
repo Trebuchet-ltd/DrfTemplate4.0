4471221e827a7c36636d9a9685499dfa5bfc5314
Adithya Nandakumar <80444800+adithyanandakumar@users.noreply.github.com>
2022-07-24 22:55:45 +0530

    Configured venv

--- config/settings.py
+++ config/settings.py
@@ -1,14 +1,16 @@
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
-if 'DJANGO_SETTINGS' in os.environ:
-    if os.environ['DJANGO_SETTINGS'] == "dev":
-        print("DEVELOPMENT SERVER")
-        from .development_settings import *
-    else:
-        print("PRODUCTION SERVER")
-        from .production_settings import *
+load_dotenv(".env")
+settings = os.getenv('SETTINGS')
+debug = os.getenv('DEBUG')
+print(settings)
+print(debug)
+
+if settings == "dev":
+    print("DEVELOPMENT SERVER")
+    from .development_settings import *
+
 else:
     print("PRODUCTION SERVER")
     from .production_settings import *
--- home/views.py
+++ home/views.py
@@ -1,3 +1,6 @@
+import urllib
+from bs4 import BeautifulSoup
+import requests as req
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.conf import settings
@@ -5,14 +8,25 @@ from home.forms import SendForm
 import yagmail
 
 
+def parse_html(link):
+    web = req.get(link)
+    S = BeautifulSoup(web.text, 'lxml')
+    return S.prettify()
+
+
 def send_mail(request):
     form = SendForm()
     if request.method == 'POST':
         form = SendForm(request.POST)
         if form.is_valid():
+            # to3 = 'sky@pip-package.com'
+            subject = "From Trebuchet 👋"
+            body = "It works, dammit!"
+            html = parse_html('https://trebuchet-thankyou.tiiny.site/')
+            # img
             recipient = form.cleaned_data.get('email')
             yag = yagmail.SMTP(settings.EMAIL_HOST_USER, oauth2_file="./credentials.json")
-            yag.send(to=[recipient], subject="From Trebuchet 👋", contents="It works, dammit!")
+            yag.send(to=[recipient], subject=subject, contents=[html])
             messages.success(request, 'Success!')
             return redirect('send_mail')
     return render(request, 'sendMail.html', {'form': form})
